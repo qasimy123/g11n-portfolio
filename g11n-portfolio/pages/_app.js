bac6d881c5b67edd2f81058945480c44d9b3227b
qasimy123 <q.invisible@hotmail.com>
2020-09-05 09:27:58 +0000

    feat: enable next-i18next to load strings

--- g11n-portfolio/pages/_app.js
+++ g11n-portfolio/pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import App from 'next/app'
+import { appWithTranslation } from '../services/i18n'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
 
-export default MyApp
+MyApp.getInitialProps = async (appContext) => ({ ...await App.getInitialProps(appContext) })
+
+export default appWithTranslation(MyApp)
