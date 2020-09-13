997fba1d43a478afe8d37373ca8f339de48f5d46
qasimy123 <q.invisible@hotmail.com>
2020-09-13 20:30:35 +0000

    feat: add new locales

--- g11n-portfolio/src/services/i18n/index.js
+++ g11n-portfolio/src/services/i18n/index.js
@@ -3,7 +3,10 @@ const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
 const path = require('path')
 
 module.exports = new NextI18Next({
-  otherLanguages: ['pk'],
+  defaultLanguage: 'en-CA',
+  otherLanguages: ['en-CA', 'fr-CA', 'ur-PK', 'en-PK'],
   localeSubpaths,
-  localePath: path.resolve('./public/static/locales')
+  localePath: path.resolve('./public/static/locales'),
+  keySeparator: '.',
+  fallbackLng:"en-CA"
 })
