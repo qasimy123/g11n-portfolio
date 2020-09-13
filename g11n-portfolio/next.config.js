a29cd48f2e362fa7b95cb522bde4e9601fdffad6
qasimy123 <q.invisible@hotmail.com>
2020-09-13 20:31:25 +0000

    feat: add locale subpaths

--- g11n-portfolio/next.config.js
+++ g11n-portfolio/next.config.js
@@ -1,6 +1,6 @@
 const { nextI18NextRewrites } = require('next-i18next/rewrites')
 
-const localeSubpaths = {}
+const localeSubpaths = {'en-CA':'en-CA', 'fr-CA':'fr-CA', 'en-PK':'en-PK', 'ur-PK': 'ur-PK'}
 
 module.exports = {
   rewrites: async () => nextI18NextRewrites(localeSubpaths),
