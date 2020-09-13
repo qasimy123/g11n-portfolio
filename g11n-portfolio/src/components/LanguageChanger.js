50afc675a021530c7954bf12016f7649d9318770
qasimy123 <q.invisible@hotmail.com>
2020-09-13 20:32:46 +0000

    feat: change languages

--- g11n-portfolio/src/components/LanguageChanger.js
+++ g11n-portfolio/src/components/LanguageChanger.js
@@ -1,10 +1,34 @@
+import { useEffect, useState } from 'react'
 import { useTranslation } from '../services/i18n'
-
 const LanguageChanger = () => {
-  const {t} = useTranslation("Hero");
-  return (<div>
-    <button>{t('language_changer_button')}</button>
-  </div>)
+  const { t, i18n } = useTranslation('Hero');
+  const [selectedLocale, setSelectedLocale] = useState()
+
+  useEffect(() => {
+    if (selectedLocale) {
+      return;
+    }
+    setSelectedLocale(i18n.language);
+  }, [selectedLocale]);
+
+  const changeLocale = (e) => {
+    e.preventDefault()
+    i18n.changeLanguage(selectedLocale)
+  }
+
+  return (
+    <div>
+      <form onSubmit={(e) => changeLocale(e)}>
+
+        <select value={selectedLocale} onChange={(e) => { setSelectedLocale(e.target.value) }}>
+          <option value="en-CA">{t('locales.en_CA')}</option>
+          <option value="en-PK">{t('locales.en_PK')}</option>
+          <option value="fr-CA">{t('locales.fr_CA')}</option>
+          <option value="ur-PK">{t('locales.ur_PK')}</option>
+        </select>
+        <button type="submit">{t('language_changer_button')}</button>
+      </form>
+    </div>)
 }
 
 export default LanguageChanger
