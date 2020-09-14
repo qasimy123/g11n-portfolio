19b2d4e9b8a5bb898f159cac2cbf26ce5287d624
qasimy123 <q.invisible@hotmail.com>
2020-09-14 02:50:50 +0000

    chore: formatting and add keys

--- g11n-portfolio/src/components/About.js
+++ g11n-portfolio/src/components/About.js
@@ -1,9 +1,19 @@
-import { withTranslation } from '../services/i18n'
+import { withTranslation } from "../services/i18n";
 
 const About = ({ t }) => (
   <div>
-    <h1>{t('about_title')}</h1>
-  </div>
-)
+    <h1>{t("about_title")}</h1>
+    <p>{t("about_paragraph")}</p>
 
-export default withTranslation('About')(About)
+    <p>{t("about_favourite_technologies")}</p>
+    <ul>
+      {t("about_favourite_technologies_list", { returnObjects: true }).map(
+        (item) => (
+          <p>{item}</p>
+        )
+      )}
+    </ul>
+  </div>
+);
+
+export default withTranslation("About")(About);
