2cc0a32d6b39006dfef8d68c4332501be7bd8f7d
qasimy123 <q.invisible@hotmail.com>
2020-09-19 20:11:25 +0000

    style: apply global styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import App from "next/app";
 import { appWithTranslation } from "../src/services/i18n";
 
+import "../styles/globals.css";
+
 const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
 
 MyApp.getInitialProps = async (appContext) => ({
