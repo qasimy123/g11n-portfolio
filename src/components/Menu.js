ecb2d86868b12787a39cc6bf100c0598c731f06c
qasimy123 <q.invisible@hotmail.com>
2020-09-22 00:54:47 +0000

    style(menu links): Underline menu links

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,27 @@
 import LanguageChanger from "./LanguageChanger";
 import { useTranslation } from "../services/i18n";
+import { extractLanguageRegion } from "../util";
+import theme from "../../styles/theme";
 
 const Menu = ({}) => {
   const { t, i18n } = useTranslation();
+  const [language, region] = extractLanguageRegion(i18n.language);
+  const currTheme = theme[region];
   return (
     <>
       <div className={"section"}>
         <h1>{t("first_name")}</h1>
         <ul className={"links"}>
           {t("socials", { returnObjects: true }).map((social) => (
-            <a href={social.url}>{social.site}</a>
+            <a
+              target="_blank"
+              and
+              rel="noopener noreferrer"
+              href={social.url}
+              className={"underline"}
+            >
+              {social.site}
+            </a>
           ))}
         </ul>
 
@@ -29,7 +41,26 @@ const Menu = ({}) => {
         .links a {
           margin-left: 0.5em;
           margin-right: 0.5em;
+          padding: 1px;
         }
+
+        .underline::after {
+          width: 0%;
+          height: 2px;
+          display: block;
+          background-color: ${currTheme.colors.text};
+          position: relative;
+          bottom: -5px;
+          content: " ";
+          transition: width 0.1s cubic-bezier(0.17, 0.67, 0.83, 0.67);
+        }
+        .underline:hover::after {
+          width: 100%;
+        }
+        .underline:focus::after {
+          width: 100%;
+        }
+
         @media screen and (max-width: 600px) {
           .section {
             flex-direction: column;
