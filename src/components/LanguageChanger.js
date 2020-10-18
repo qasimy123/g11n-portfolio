87baad24ae10eaf07f482fbb2c3e2c5caa82aec0
qasimy123 <q.invisible@hotmail.com>
2020-10-18 09:43:54 +0000

    style(language changer): Update to use lottie

--- src/components/LanguageChanger.js
+++ src/components/LanguageChanger.js
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
+import Lottie from "react-lottie";
 import { useTranslation } from "../services/i18n";
 import theme from "../../styles/theme";
 import { extractLanguageRegion } from "../util";
+import languageLoadingAnimationData from "../lotties/language-loading.json";
+
 const LanguageChanger = () => {
   const { t, i18n } = useTranslation("Hero");
   const [language, region] = extractLanguageRegion(i18n.language);
@@ -9,7 +12,14 @@ const LanguageChanger = () => {
   const dir = i18n.dir(language);
   const [selectedLocale, setSelectedLocale] = useState();
   const [loading, setLoading] = useState(false);
-
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: languageLoadingAnimationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   useEffect(() => {
     if (selectedLocale) {
       return;
@@ -27,22 +37,29 @@ const LanguageChanger = () => {
     <>
       <div className={"language-changer"}>
         {loading ? (
-          <p>{t("loading")}</p>
+          <Lottie width={70} options={defaultOptions} />
         ) : (
-          <select
-            dir={dir}
-            className="select-language"
-            value={selectedLocale}
-            onChange={(e) => {
-              changeLocale(e.target.value);
-              setSelectedLocale(e.target.value);
-            }}
-          >
-            <option value="en-CA">{t("locales.en_CA")}</option>
-            <option value="fr-CA">{t("locales.fr_CA")}</option>
-            <option value="en-PK">{t("locales.en_PK")}</option>
-            <option value="ur-PK">{t("locales.ur_PK")}</option>
-          </select>
+          <div className={"language-select-container"}>
+            <img
+              dir={dir}
+              className={"flag-icon"}
+              src={`static/icons/${region}.svg`}
+            ></img>
+            <select
+              dir={dir}
+              className="select-language"
+              value={selectedLocale}
+              onChange={(e) => {
+                changeLocale(e.target.value);
+                setSelectedLocale(e.target.value);
+              }}
+            >
+              <option value="en-CA">{t("locales.en_CA")}</option>
+              <option value="fr-CA">{t("locales.fr_CA")}</option>
+              <option value="en-PK">{t("locales.en_PK")}</option>
+              <option value="ur-PK">{t("locales.ur_PK")}</option>
+            </select>
+          </div>
         )}
       </div>
       <style jsx>{`
@@ -51,8 +68,21 @@ const LanguageChanger = () => {
           flex-direction: column;
           justify-content: center;
           min-width: 50px;
+          max-height: 70px;
         }
-
+        .flag-icon {
+          width: 50px;
+          border-radius: 100em;
+        }
+        .flag-icon[dir="ltr"] {
+          margin-right: 5px;
+        }
+        .flag-icon[dir="rtl"] {
+          margin-left: 5px;
+        }
+        .language-select-container {
+          display:flex;
+        }
         .select-language {
           display: block;
           font-size: 16px;
@@ -71,13 +101,13 @@ const LanguageChanger = () => {
           background-color: ${currTheme.colors.white};
         }
         .select-language:focus {
-          color: ${currTheme.colors.paper};
+          color: ${currTheme.colors.text};
           outline: none;
         }
 
         @media screen and (max-width: 600px) {
           .select-language {
-            width: 60%;
+            width: 100%;
             text-align-last: center;
           }
           .language-changer {
@@ -87,7 +117,7 @@ const LanguageChanger = () => {
         }
 
         .select-language:hover {
-          box-shadow: inset 0px 5px 16px -7px rgba(0, 0, 0, 0.57);
+          box-shadow: inset 0px 0px 0px 3px  ${currTheme.colors.primaryColor};
           cursor: pointer;
         }
         .select-language[dir="rtl"] {
